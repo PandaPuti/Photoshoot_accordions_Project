9eaa999c92ca6b506b82ec7b11b6c84e1eca4fc2
PandaPuti <86776469+PandaPuti@users.noreply.github.com>
2025-03-18 17:47:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const items = document.querySelectorAll(".accordion-item");
+
+const resetItems = () => {
+    items.forEach(
+        item => item.classList.remove("active")
+    );
+};
+
+const handleClick = (element , index) => {
+    resetItems();
+
+    element.classList.add("active");
+    
+    localStorage.setItem("accordionIndex", index);
+};
+
+resetItems();
+
+const activeItem = items[localStorage.getItem("accordionIndex")];
+
+if(activeItem){
+    activeItem.classList.add("active");
+}else{
+    items[0].classList.remove("active");
+}
